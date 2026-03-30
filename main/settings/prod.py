0d8a65629ef65b5f718b968fdfe9b30695130860
academiahye <hyeacademydigital@gmail.com>
2026-03-30 18:50:21 -0500

    Integracion sendgrid api para email final

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -9,12 +9,3 @@ X_FRAME_OPTIONS = "DENY"
 SECURE_SSL_REDIRECT = True
 
 
-# ==========================
-# CELERY + REDIS
-# ==========================
-
-CELERY_BROKER_URL = os.environ.get("REDIS_URL")
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_BACKEND = "django-db"
-CELERY_TIMEZONE = "America/Lima"
