e624527755bd1bb87173ae0e06d73170d104d17f
academiahye <hyeacademydigital@gmail.com>
2026-03-30 19:14:49 -0500

    Integracion sendgrid api para email final request requirements  gggg requiements

--- main/settings/base.py
+++ main/settings/base.py
@@ -147,13 +147,13 @@ LOGIN_URL = "usuarios:iniciar_sesion"
 LOGIN_REDIRECT_URL = "core:inicio"
 LOGOUT_REDIRECT_URL = "core:inicio"
 
-EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = config("EMAIL_HOST", default="localhost")
-EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="no-reply@iestparib.edu.pe")
+#EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
+#EMAIL_HOST = config("EMAIL_HOST", default="localhost")
+#EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
+#EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+#EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+#EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+#DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="no-reply@iestparib.edu.pe")
 
 #cONFIGURACION DE GOOGLE DRIVE
 GOOGLE_DRIVE_SERVICE_ACCOUNT_FILE = config("GOOGLE_DRIVE_SERVICE_ACCOUNT_FILE", default="")
