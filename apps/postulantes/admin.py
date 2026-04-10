ddb4e747103be4383590526e0143d41f2c40c1c6
academiahye <hyeacademydigital@gmail.com>
2026-04-10 11:20:53 -0500

    visualizacion de inscripcion

--- apps/postulantes/admin.py
+++ apps/postulantes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from apps.postulantes.models import Postulante
+from apps.postulantes.models import Inscripcion
 # Register your models here.
 admin.site.register(Postulante)
+admin.site.register(Inscripcion)
